351f084c8f055c7fb007861483385883917593c3
mika <mikagivner123@gmail.com>
2023-03-27 23:27:36 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import "./App.css";
 import io from "socket.io-client";
 import { useState, useEffect } from "react";
 
-const socket = io.connect("https://songs-gusses.onrender.com");
+const socket = io.connect("https://songs-gusses.onrender.com", {
+  transports: ["websocket"],
+});
 // const socket = io.connect("http://localhost:5000");
 // const socket = io.connect("https://extraordinary-rugelach-ce47fd.netlify.app");
 
