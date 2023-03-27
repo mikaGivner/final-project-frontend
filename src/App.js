be4bb44f0ecd612ae48b3a0df67314e53f5d80d5
mika <mikagivner123@gmail.com>
2023-03-27 23:10:09 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import "./App.css";
 import io from "socket.io-client";
 import { useState, useEffect } from "react";
 
-// const socket = io.connect("https://songs-gusses.onrender.com/");
+const socket = io.connect("https://songs-gusses.onrender.com/");
 // const socket = io.connect("http://localhost:5000");
-const socket = io.connect("https://extraordinary-rugelach-ce47fd.netlify.app");
+// const socket = io.connect("https://extraordinary-rugelach-ce47fd.netlify.app");
 
 function App() {
   const [room, setRoom] = useState("");
