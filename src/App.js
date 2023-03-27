943fd8cbdf33df342f5c8af03669071719074c8c
mika <mikagivner123@gmail.com>
2023-03-27 22:36:02 +0300

    frontend demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
+
+const socket = io.connect("https://songs-gusses.onrender.com/");
+// const socket = io.connect("http://localhost:5000");
 
 function App() {
+  const [room, setRoom] = useState("");
+  const [participantsCount, setParticipantsCount] = useState(4);
+  const [newName, setNewName] = useState("");
+
+  const [joinsPeople, setJoinsPeople] = useState("");
+  const [goRoom, setGoRoom] = useState(false);
+
+  const UpdateName = (e) => {
+    setNewName(e.target.value);
+  };
+  const UpdateRoom = (e) => {
+    setRoom(e.target.value);
+    setGoRoom(false);
+  };
+  // const joinRoom=()=>{
+  //   if(room!==""){
+  //     socket.emit("join_room", room);
+  //   }
+  // };
+
+  const JoinGame = () => {
+    setGoRoom(true);
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+    socket.emit("add_participant", newName);
+    setParticipantsCount(Number(participantsCount) - 1);
+  };
+
+  useEffect(() => {
+    socket.on("participant_added", (data) => {
+      setParticipantsCount(Number(participantsCount) - 1);
+      setJoinsPeople(joinsPeople + data);
+    });
+  }, [joinsPeople, participantsCount]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Choose a name:</label>
+      <input type="text" value={newName} onChange={UpdateName} />
+      <div>
+        <label>Choose a room:</label>
+        <input type="number" value={room} onChange={UpdateRoom} />
+      </div>
+      <button onClick={JoinGame}>Enter Room</button>
+      <div>Participants Count : {participantsCount}</div>
+
+      {goRoom && room && (
+        <div>
+          join to room{room}:{newName},{joinsPeople}
+        </div>
+      )}
     </div>
   );
 }
