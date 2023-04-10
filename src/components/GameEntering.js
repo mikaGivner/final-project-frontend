0859e1bb44279a0b80235a5cd4299a79c7a61840
mika <mikagivner123@gmail.com>
2023-04-10 16:47:56 +0300

    change the path

--- src/components/GameEntering.js
+++ src/components/GameEntering.js
@@ -77,11 +77,11 @@ export default function GameEntering() {
   }, [joinsPeople]);
   useEffect(() => {
     socket.on("participant_added", (data) => {
-      setJoinsPeople([...joinsPeople, data]);
+      setJoinsPeople([...data]);
     });
     return () => {
       socket.off("participant_added", (data) => {
-        setJoinsPeople([...joinsPeople, data]);
+        setJoinsPeople([...data]);
       });
     };
     // const handleParticipantAdded = (data) => {
