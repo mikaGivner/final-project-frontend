66260e35d884f93cdeb60d19a02f928d9cede8b8
mika <mikagivner123@gmail.com>
2023-04-13 17:10:13 +0300

    only one admin

--- src/components/EnteringPage.js
+++ src/components/EnteringPage.js
@@ -20,9 +20,7 @@ export default function EnteringPage() {
           joinsPeople.length !== 0 &&
           joinsPeople.map((user) => {
             if (user.isAdmin)
-              return (
-                <div style={{ fontWeight: "bold" }}>Admin: {user.name}</div>
-              );
+              return <div style={{ fontWeight: "bold" }}>{user.name}</div>;
             else return <div>{user.name}</div>;
           })}
       </ParticipantsPresentationStyle>
