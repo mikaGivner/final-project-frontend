b4fe5cb8f26f5e931cb548c37e6a82d98dc1a621
mika <mikagivner123@gmail.com>
2023-04-13 17:21:35 +0300

    only one admin

--- src/components/GameEntering.js
+++ src/components/GameEntering.js
@@ -37,7 +37,7 @@ export default function GameEntering() {
   const [thePin, setThePin] = useState("");
 
   const PinRender = localStorage.getItem("isAdmin");
-
+  const userConnected = localStorage.getItem("user");
   const PinFun = async () => {
     let result = "";
     let characters =
@@ -100,51 +100,56 @@ export default function GameEntering() {
     setPinError("");
     const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{2,10}$/;
     //checking a name
-    if (!newName) setNameError("Please choose a name");
-    else if (!regex.test(newName))
-      setNameError(
-        "Please check if the name: contain a number, upper and lower case and in length between 2-10 characters "
-      );
-    else greatName = true;
-    //checking a pin
-    if (!newPin) setPinError("Please enter a pin");
-    else {
-      try {
-        await axios.get(
+    if (userConnected.length === 0) {
+      setNameError("Plese check that you didnt connect from other tab");
+    } else {
+      if (!newName) setNameError("Please choose a name");
+      else if (!regex.test(newName))
+        setNameError(
+          "Please check if the name: contain a number, upper and lower case and in length between 2-10 characters "
+        );
+      else greatName = true;
+      //checking a pin
+      if (!newPin) setPinError("Please enter a pin");
+      else {
+        try {
+          await axios.get(
+            `https://songs-gusses.onrender.com/api/v1/newPlay/${newPin}`
+          );
+          greatPin = true;
+        } catch {
+          setPinError("This pin is not exist");
+        }
+      }
+      if (greatName && greatPin) {
+        let game = await axios.get(
           `https://songs-gusses.onrender.com/api/v1/newPlay/${newPin}`
         );
-        greatPin = true;
-      } catch {
-        setPinError("This pin is not exist");
-      }
-    }
-    if (greatName && greatPin) {
-      let game = await axios.get(
-        `https://songs-gusses.onrender.com/api/v1/newPlay/${newPin}`
-      );
-      if (game.data.data.participants.includes(newName)) {
-        setNameError("This name exists in the game you are trying to access");
-      } else {
-        const updatedPartisipants = [...game.data.data.participants, newName];
-        await axios.put(
-          `https://songs-gusses.onrender.com/api/v1/newPlay/${newPin}`,
-          {
-            participants: updatedPartisipants,
-          }
-        );
-        if (newPin === PinRender) {
+        if (game.data.data.participants.includes(newName)) {
+          setNameError("This name exists in the game you are trying to access");
+        } else {
+          const updatedPartisipants = [...game.data.data.participants, newName];
           await axios.put(
             `https://songs-gusses.onrender.com/api/v1/newPlay/${newPin}`,
             {
-              admin: newName,
+              participants: updatedPartisipants,
             }
           );
-          admin = true;
-        }
-        setGoRoom(true);
-        socket.emit("join_room", newPin, newName);
+          if (newPin === PinRender) {
+            await axios.put(
+              `https://songs-gusses.onrender.com/api/v1/newPlay/${newPin}`,
+              {
+                admin: newName,
+              }
+            );
+            admin = true;
+          }
+          setGoRoom(true);
+          localStorage.setItem("user", newName);
+          socket.emit("join_room", newPin, newName);
 
-        socket.emit("add_participant", newName, newPin, admin);
+          socket.emit("add_participant", newName, newPin, admin);
+        }
       }
     }
   };
