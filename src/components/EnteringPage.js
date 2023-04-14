d975408e0b23d4316bcc6b89208df976709a8408
mika <mikagivner123@gmail.com>
2023-04-14 14:01:08 +0300

    find admin from db

--- src/components/EnteringPage.js
+++ src/components/EnteringPage.js
@@ -1,18 +1,22 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import {
   EnteringPageStyle,
   ParticipantsPresentationStyle,
 } from "../style/Index";
 import { StatesContext } from "../ContextFile";
 import "../AnimationsAndDefineds.css";
-// import Inputs from "./Inputs";
-//import Btn from "./Btn";
+import axios from "axios";
 
 export default function EnteringPage() {
   //const placeHolders = ["Choose a name", "Enter your pin"];
   const { newName, newPin, joinsPeople, goRoom } = useContext(StatesContext);
   const PinRender = localStorage.getItem("isAdmin");
   const adminName = localStorage.getItem("nameAdmin");
+  useEffect(() => {
+    const myGame = axios.get(
+      `https://songs-gusses.onrender.com/api/v1/newPlay/${newPin}`
+    );
+  }, []);
   return (
     <EnteringPageStyle>
       <h1 style={{ color: "#000" }}>Participants for this game</h1>
@@ -20,7 +24,8 @@ export default function EnteringPage() {
         {goRoom &&
           joinsPeople.length !== 0 &&
           joinsPeople.map((user) => {
-            if (user.isAdmin && user.userAdmin === adminName)
+            // if (user.isAdmin && user.userAdmin === adminName)
+            if (myGame.data.data.admin === user.name)
               return <div style={{ fontWeight: "bold" }}>{user.name}</div>;
             else return <div>{user.name}</div>;
           })}
