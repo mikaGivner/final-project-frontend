e0d97258e425025a06c027e4c7f2699b9f77b642
mika <mikagivner123@gmail.com>
2023-04-16 16:06:25 +0300

    add start  page

--- src/App.js
+++ src/App.js
@@ -56,13 +56,13 @@ function App() {
     socket.emit("game_started", true, newPin);
   }, [newPin]);
 
-  socket.on("start", (data) => {
-    console.log("data:", data);
-    if (data) setIsGameStarted(true);
-  });
-  // useEffect(() => {
-  //   //console.log("is game:", isGameStarted);
-  // }, [setIsGameStarted]);
+  useEffect(() => {
+    socket.on("start", (data) => {
+      console.log("data:", data);
+      if (data) setIsGameStarted(true);
+    });
+    //   //console.log("is game:", isGameStarted);
+  }, [setIsGameStarted]);
   useEffect(() => {
     console.log("is game:", isGameStarted);
   }, [isGameStarted]);
