56cba8c58e465273fe6a99f853adfcfcab95e78d
mika <mikagivner123@gmail.com>
2023-04-18 14:41:50 +0300

    new open page

--- src/style/GameEnteringStyle.js
+++ src/style/GameEnteringStyle.js
@@ -3,11 +3,12 @@ import "../AnimationsAndDefineds.css";
 export const GameEnteringStyle = styled.div`
   padding: 3rem;
   margin-right: 2rem;
-  width: 35vw;
-  height: 87vh;
+  width: 100vm;
+  height: 100vm;
   background: #000;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
+  align-items: center;
   color: #fff;
 `;
